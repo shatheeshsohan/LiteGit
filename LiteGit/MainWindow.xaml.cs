278fd088d7593cffa3bd9d7bee231243df74cf8f
ifs-satrlk <satheesh.rathnasiri@ifsworld.com>
2024-02-03 11:43:44 +0530

    Base Development - Phase 1

---
+++ LiteGit/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using LibGit2Sharp;
+using LiteGit.Model;
+using LiteGit.Util;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LiteGit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AppInit();
+            UIInit();
+        }
+
+        private void AppInit()
+        {
+            try
+            {
+                var lines = File.ReadLines(@"C:\IFS\litegit.properties");
+                var data = lines.Select(l => l.Split('='));
+
+                List<BaseComboItem> allReleasePaths = data.Where(arr => arr.Length > 0).Select(arr => new BaseComboItem
+                {
+                    Release = arr[0].Trim(),
+                    Path = arr[1].Trim()
+                }).ToList();
+
+                foreach (BaseComboItem item in allReleasePaths)
+                {
+                    BaseCombo.Items.Add(item.Path);
+                }
+
+                BaseCombo.SelectedIndex = 0;
+                FetchedCurrentBranch.Content = "Current branch is not fetched yet.";
+            }
+            catch (FileNotFoundException e)
+            {
+                System.Windows.Forms.MessageBox.Show("litegit.properties not cound in C/IFS", "File Not Found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(0);
+            }
+
+        }
+
+        private void UIInit()
+        {
+            this.BottomStatusBar.BorderThickness = new Thickness(0.5);
+            string currentRepo = (string)BaseCombo.SelectedItem;
+            RepoUtils.GetAllLocalBranches(currentRepo).ForEach(item => LocalBranchesListBox.Items.Add(item));
+        }
+
+        private void StatusButton_Click(object sender, RoutedEventArgs e)
+        {
+            FetchedCurrentBranch.Content = RepoUtils.GetCurrentBranch((string)BaseCombo.SelectedItem);
+            CheckOutTextBox.Text = (string)FetchedCurrentBranch.Content;
+        }
+
+        private void BaseCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string currentRepo = (string)BaseCombo.SelectedItem;
+            FetchedCurrentBranch.Content = RepoUtils.GetCurrentBranch(currentRepo);
+            LocalBranchesListBox.Items.Clear();
+            RepoUtils.GetAllLocalBranches(currentRepo).ForEach(item => LocalBranchesListBox.Items.Add(item));
+            LocalBranchesListBox.SelectedIndex = 0;
+            CheckOutTextBox.Clear();
+        }
+
+        private void CheckoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string currentRepo = (string)BaseCombo.SelectedItem;
+                var repo = new Repository(currentRepo);
+                RepoUtils.CheckoutBranch(repo, CheckOutTextBox.Text);
+            }
+            catch (Exception)
+            {
+                System.Windows.Forms.MessageBox.Show("Please verify the checkout branch", "Branch Not Found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void LocalBranchesListBox_MouseDoubleClick(object sender, RoutedEventArgs e)
+        {
+            CheckOutTextBox.Text = (string) LocalBranchesListBox.SelectedItem;
+        }
+
+        private void LocalBranchesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void PullButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string currentRepo = (string)BaseCombo.SelectedItem;
+                var repo = new Repository(currentRepo);
+                RepoUtils.Pull(repo, CheckOutTextBox.Text);
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message, "Pull Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ LiteGit/Util/RepoUtils.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibGit2Sharp;
+namespace LiteGit.Util
+{
+    class RepoUtils
+    {
+        public static string GetCurrentBranch (string repoDir) {
+            var repo = new Repository(repoDir);
+            if (repo != null)
+            {
+                string repoName = repo.Head.FriendlyName;
+                repo.Dispose();
+                return repoName;
+            }
+            return "";
+        }
+
+        public static List<string> GetAllLocalBranches(string repoDir)
+        {
+            var repo = new Repository(repoDir);
+            List<string> allLocalBranches = new List<string>();
+            if (repo != null)
+            {
+                allLocalBranches = repo.Branches.Where(branch => branch.IsRemote == false).Select(branch => branch.FriendlyName).ToList();
+                repo.Dispose();
+            }
+            return allLocalBranches;
+        }
+
+        public static void Pull(Repository repository, string branch)
+        {
+            try
+            {
+                PullOptions pullOptions = new PullOptions()
+                {
+                    MergeOptions = new MergeOptions()
+                    {
+                        FastForwardStrategy = FastForwardStrategy.Default
+                    }
+
+
+                };
+                MergeResult mergeResult = Commands.Pull(
+                    repository,
+                    new Signature("ifs-satrlk", "satheesh.rathnasiri@ifs.com", DateTimeOffset.Now),
+                    pullOptions
+                );
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public static string CheckoutBranch(Repository repository, string branchName)
+        {
+            try
+            {
+                var trackingBranch = repository.Branches[branchName];
+                if (trackingBranch.IsRemote)
+                {
+                    branchName = branchName.Replace("origin/", string.Empty);
+                    var branch = repository.CreateBranch(branchName, trackingBranch.Tip);
+                    repository.Branches.Update(branch, b => b.TrackedBranch = trackingBranch.CanonicalName);
+                    Commands.Checkout(repository, branch, new CheckoutOptions { CheckoutModifiers = CheckoutModifiers.Force });
+                }
+                else
+                {
+                    Commands.Checkout(repository, trackingBranch, new CheckoutOptions { CheckoutModifiers = CheckoutModifiers.Force });
+                }
+                return branchName;
+            }
+            catch (Exception e) 
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
